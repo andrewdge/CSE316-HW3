2993228b624300d191d4cf41dfcdc92f2ad53a5b
Andrew Ge <andrewdge411@gmail.com>
2021-04-07 15:20:02 -0400

    stopped spammability of sidebar which caused issue

--- client/src/components/sidebar/SidebarEntry.js
+++ client/src/components/sidebar/SidebarEntry.js
@@ -5,6 +5,8 @@ const SidebarEntry = (props) => {
     const [editing, toggleEditing] = useState(false);
     const [preEdit, setPreEdit] = useState(props.name);
 
+    const enabled = props._id === props.activeid;
+
     const handleEditing = (e) => {
         e.stopPropagation();
         setPreEdit(props.name);
@@ -21,8 +23,8 @@ const SidebarEntry = (props) => {
     
     return (
         <WNavItem 
-            className={entryStyle} onDoubleClick={handleEditing} 
-            onClick={() => { props.handleSetActive(props._id, props.activeid) }} hoverAnimation="lighten"
+            className={entryStyle} onDoubleClick={enabled ? handleEditing : () => {}} 
+            onClick={!enabled ? () => { props.handleSetActive(props._id, props.activeid)} : () => {} } hoverAnimation="lighten"
         >
             {
                 editing ? <WInput className="list-item-edit" inputClass="list-item-edit-input" wType="lined" barAnimation="solid" name='name' onBlur={handleSubmit} autoFocus={true} defaultValue={props.name} />
